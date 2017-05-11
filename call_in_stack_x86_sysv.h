43a8c19564fbfd58add40954155bb42c41991f24
yuanzhu <41656955@qq.com>
2017-05-11 23:13:24 +0800

    x86 remove padding set

--- call_in_stack_x86_sysv.h
+++ call_in_stack_x86_sysv.h
@@ -28,7 +28,6 @@ namespace call_in_stack_impl{
 		\
 		const static word_int_t addtional_stack_cost = _COUNT_OF_SIZE(change_ref_to_pointer_size<new_type>::size, WORDSIZE);\
 		const static word_int_t stackword_cost = parent::stackword_cost + ((addtional_stack_cost == 0)? 0 : (_ALIGNED_COST(parent::stackword_cost, addtional_stack_cost)));\
-		const static word_int_t stack_padding_reporter = (1<<stackword_cost) -1;\
 		\
 		typedef assert_not_class_not_largesize<new_type, MAX_ARGUMENT_SIZE> assert_instance;\
 		template<typename O>\
@@ -36,22 +35,15 @@ namespace call_in_stack_impl{
 			o<<"stackword_cost at " <<i<<"th:"<<stackword_cost<<"\r\n";\
 			parent::out_stackword_cost(o);\
 		}\
-		template<typename O>\
-		static void out_stack_padding_reporter(O& o){\
-			o<<"stack_padding_reporter at " <<i<<"th:"<<stack_padding_reporter<<"\r\n";\
-			parent::out_stack_padding_reporter(o);\
-		}\
 	};
 
 	BATCH_FUNC1(args_list_define)
 
-	//((arg_types::stack_padding_reporter & (((word_int_t)1)<<((word_int_t)(arg_types::stackword_cost-j))%WORDBITSIZE)) != 0) means the space is for padding(and lea is cheaper than push)
-	// Here "%WORDBITSIZE" is to escape from shifting overflow warning...
+    #undef args_list_define
+
 
 	#define push_stack_define(j) if(arg_types::stackword_cost >= j ){\
-		if((arg_types::stack_padding_reporter & (((word_int_t)1)<<((word_int_t)(arg_types::stackword_cost-j))%WORDBITSIZE)) != 0) \
-				__asm__ ("push "MACRO_TOSTRING(j*WORDSIZE+WORDSIZE)"(%ebp);\n\t");\
-		else 	__asm__ ("lea -"MACRO_TOSTRING(WORDSIZE)"(%esp), %esp;\n\t");}
+       __asm__ ("push "MACRO_TOSTRING(j*WORDSIZE+WORDSIZE)"(%ebp);\n\t");}
 
 	//MAX_ARGUMENT_SIZE = 3*WORDSIZE, 10*3=30,
 	#define func_back1(func)  func(30) func(29) func(28) func(27) func(26) func(25) func(24) func(23) func(22) func(21) func(20) func(19) func(18) func(17) func(16) func(15) func(14) func(13) func(12) func(11) func(10) func(9) func(8) func(7) func(6) func(5) func(4) func(3) func(2) func(1)
@@ -62,7 +54,7 @@ namespace call_in_stack_impl{
 	#define STACK_COST(T) (T::stackword_cost + 1)
 
 	#define call_with_stack_define(i) \
-	template <MACRO_JOIN(RECURSIVE_FUNC_,i)(define_typenames_begin, define_typenames, define_typenames)typename R, bool has_variable_arguments > \
+	template <MACRO_JOIN(RECURSIVE_FUNC_,i)(define_typenames_begin, define_typenames, define_typenames) typename R, bool has_variable_arguments > \
 	FORCE_NOINLINE DLL_LOCAL R do_call (\
 		MACRO_JOIN(RECURSIVE_FUNC_,i)(define_types_begin, define_types, define_types)  \
 		void* dest_func, char* stack_base){\
